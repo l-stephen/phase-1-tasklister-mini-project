495a4aef0c0f9c1280476fd9040be1fdc69e6cb8
Stephen Lambert <97977357+l-stephen@users.noreply.github.com>
2023-09-14 16:08:57 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.querySelector('#create-task-form')
+  const button = document.createElement("button")
+  button.textContent = "X"
 
   form.addEventListener('submit', (event) => {
     event.preventDefault()
     const taskTxt = event.target["new-task-description"].value
-
-    const list = document.querySelector('#tasks')
+  
+    // button.addEventListener("click", (e)=>{
+    //   const li = e.target.parentNode
+    //   li.remove()
+    // })
+   
     const newLi = document.createElement('li')
     newLi.textContent = taskTxt
+    const list = document.querySelector('#tasks')
+    const tasks = document.querySelector()
+    
 
     list.appendChild(newLi)
+    list.appendChild(button)
   })
 });
